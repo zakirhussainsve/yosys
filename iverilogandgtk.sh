0975f27b1c9fd359320b91bd46ad9fe9c0c606b7
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-21 06:02:41 +0000

    iverilogandgtkwave setup

---
+++ iverilogandgtk.sh
@@ -0,0 +1,9 @@
+##Install Icarus iverilog
+echo "*****************************************  Installing iverilog ***********************************************"
+echo
+echo
+sudo apt-get install -y iverilog --assume-yes
+echo "****************************************** Installing gtkwave ********************************************"
+echo
+echo
+sudo apt install gtkwave --assume-yes
